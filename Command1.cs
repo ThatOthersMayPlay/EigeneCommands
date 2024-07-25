9508c76a33a913f9a54cf285be0ad854ee206c98
ThatOthersMayPlay <85338140+ThatOthersMayPlay@users.noreply.github.com>
2024-07-25 12:38:12 +0200

    Projektdateien hinzufügen.

---
+++ Command1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EigeneCommands
+{
+    // Erstellung der Command1 - Klasse, die das Interface ICommand implementiert 
+    public class Command1 : ICommand
+    {
+        // Variable _execute vom Typ Action kann eine Methode zugewiesen werden,
+        // die keine Parameter hat und keinen Wert zurückgibt 
+        // Variablen kann nur im Konstruktor einen Wert zurückgewiesen bekommen
+        // und danach nur noch gelesen werden 
+        private readonly Action _execute;
+        // Variable _canExecute vom Typ Func<bool> kann eine Methode zugewiesen werden,
+        // die keine Parameter hat und einen bool zurück gibt
+        private readonly Func<bool> _canExecute;
+
+        // 2. Parameter vom Konstruktor ist optional
+        public Command1(Action execute, Func<bool> canExecute=null)
+        {
+            // _execute auf NULL überprüft
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute/* ?? throw new ArgumentNullException(nameof(canExecute))*/;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {   // Was Verwendung der Klasse CommandManager 
+            // CommandManager verwendet, um Ereignismethoden hinzuzufügen
+            // und wieder zu löschen
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+            return _canExecute();
+            //return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            // Ausführen der Action bzw. ausführen der Methode,
+            // der der Variablen vom Typ Action zugewiesen wurde 
+            _execute();
+        }
+    }
+
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
+
+namespace EigeneCommands
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand CloseCommand {  get; }
+        public MainViewModel()
+        {
+            inhalt = "";
+            CloseCommand = new Command1(CloseApplication, CanCloseApplication);
+        }
+
+        private string inhalt;
+
+        public string Inhalt
+        {
+            get { return inhalt; }
+            set
+            {
+                if (inhalt != value)
+                {
+                    inhalt = value;
+                    OnPropertyChanged(nameof(inhalt));
+                    CommandManager.InvalidateRequerySuggested();
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+
+        private bool CanCloseApplication()
+        {
+            //return true;
+            return Inhalt.ToLower() == "ende";
+        }
+
+        private void CloseApplication()
+        {
+            Application.Current.Shutdown();
+            //MessageBox.Show("hey");
+        }
+
+    }
+}
